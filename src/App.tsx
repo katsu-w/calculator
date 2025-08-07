9419ef935fc082ac29ef19f383d144c32460a2d9
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-08-08 01:11:05 +0300

    calculator html + functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,114 @@
 import './styles/main.scss'
+import { useState } from 'react';
+
+const BUTTONS: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '=', '+',];
 
 export function App() {
+	const [firstOperand, setFirstOperand] = useState('');
+	const [secondOperand, setSecondOperand] = useState('');
+	const [operator, setOperator] = useState('');
+	const [error, setError] = useState('');
+	const [isResult, setIsResult] = useState(false);
+	
+	function setDisplay(char: string) {
+		if (!operator) {
+			setIsResult(false)
+			setFirstOperand(val => val + char);
+			setError('');
+			return;
+		} else {
+			setSecondOperand(val => val + char);
+			setError('');
+			return;
+		}
+	}
+	
+	function equals() {
+		switch (operator) {
+			case '/':
+				setFirstOperand(String(+firstOperand / +secondOperand));
+				break;
+			case '*':
+				setFirstOperand(String(+firstOperand * +secondOperand));
+				break;
+			case '-':
+				setFirstOperand(String(+firstOperand - +secondOperand));
+				break;
+			case '+':
+				setFirstOperand(String(+firstOperand + +secondOperand));
+				break;
+		}
+		
+		setOperator('');
+		setSecondOperand('');
+		setIsResult(true);
+	}
+	
+	function handleClick(char: string) {
+		switch (char) {
+			case '/':
+			case '*':
+			case '-':
+			case '+':
+				if (firstOperand) {
+					setOperator(char);
+				} else {
+					setError('Сначала добавьте операнд')
+				}
+				break;
+				
+			case 'C':
+				setFirstOperand('');
+				setSecondOperand('');
+				setOperator('');
+				break;
+			
+			case '=':
+				if (secondOperand) {
+					equals();
+					break;
+				} else if (operator) {
+					setError('Использован неверный формат');
+					break;
+				}
+				break;
+				
+			
+			default:
+				setDisplay(char);
+				break;
+		}
+		
+	}
 
   return (
     <>
-			
+			<div className="calculator">
+				<div className="calculator__display">
+					<span className={`calculator__text${isResult ? ' result' : ''}`}>
+						{firstOperand + ' ' + operator + ' ' + secondOperand}
+					</span>
+				</div>
+				<div className="calculator__error">
+					<span>{error}</span>
+				</div>
+				<div className="calculator__buttons">
+					{
+						BUTTONS.map((name) => {
+							return (
+								<button
+									className="button"
+									onClick={() => handleClick(name)}
+									type="button"
+									key={name}
+								>
+									{name}
+								</button>
+							);
+						})
+					}
+				</div>
+			</div>
     </>
   )
 }
